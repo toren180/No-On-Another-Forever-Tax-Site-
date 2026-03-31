3901e384fdd40e7db869cef0373caa9749015949
Toren Snyder <snydertoren@gmail.com>
2026-03-31 19:48:28 -0400

    fix bug with form on volunteer page

--- js/form-config.js
+++ js/form-config.js
@@ -4,6 +4,6 @@
  * Set formSecret to match Script property FORM_SECRET (Project Settings > Script properties).
  */
 window.SHEET_FORM_CONFIG = {
-    webAppUrl: 'https://script.google.com/macros/s/.../exec',
+    webAppUrl: 'https://script.google.com/macros/s/AKfycbx1oHy0ZEguaJAhOHs_jvUvHYnyTDqkx3dEShLlTs6d3XD7a8WkseFxdMkFFynOxaebOQ/exec',
     formSecret: ''
 };
